45e802200fdadda59fc9b9052853364cca273ee2
Xris <cp@maxfractal.com>
2022-03-17 00:42:38 -0400

    Update wumpus.py
    Bug fixes: raw_input is now input for Python 3.x

--- wumpus.py
+++ wumpus.py
@@ -6,25 +6,25 @@ player_location = choice(cave_numbers)
 while player_location == wumpus_location:
     player_location = choice(cave_numbers)
 
-print "Welcome to Hunt the Wumpus!"
-print "You can see ", len(cave_numbers), " caves."
-print "To play, just type the number"
-print "of the cave you wish to enter next."
+print ("Welcome to Hunt the Wumpus!");
+print ("You can see ", len(cave_numbers), " caves.");
+print ("To play, just type the number");
+print ("of the cave you wish to enter next.");
 
 while True:
-    print "You are in a cave", player_location
+    print ("You are in a cave", player_location);
     if (player_location == wumpus_location - 1 or
-    or player_location == wumpus_location + 1):
-    print "I smell a wumpus..."
+        player_location == wumpus_location + 1):
+        print ("I smell a wumpus...");
 
-    print "Which cave next?"
-    player_input = raw_input(">")
+    print ("Which cave next?");
+    player_input = input(">")
     if (not player_input.isdigit() or
         int(player_input) not in cave_numbers):
-        print player_input, " is not a cave!"
+        print (player_input, " is not a cave!");
 
     else: 
         player_location = int(player_input)
         if player_location == wumpus_location:
-            print "Aargh! You got eaten by a wumpus!"
+            print ("Aargh! You got eaten by a wumpus!");
             break
