5b9aeec96554ead5d48cf2ce3b6c2c335a37ed51
Xris <cp@maxfractal.com>
2022-03-29 19:41:32 -0400

    Array.append
    Add Rope to array.

---
+++ f003.py
@@ -0,0 +1,11 @@
+from foo import *
+import subprocess
+
+print("start");
+print(first_three);
+print("  from f003  ");
+print(last_three);
+subprocess.call("./foo2.sh", shell=True);
+wumpus_r_us_list.append('Rope');
+print (wumpus_r_us_list);
+print("end.");
