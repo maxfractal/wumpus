812f7feaca756040b99da70e3336b4da4df3aecd
Xris <cp@maxfractal.com>
2023-03-09 13:23:51 -0500

    Adding text adventure file.

--- f003.py
+++ f003.py
@@ -2,9 +2,9 @@ from foo import *
 import subprocess
 
 print("start");
-print(first_three);
+#print(first_three);
 print("  from f003  ");
-print(last_three);
+#print(last_three);
 subprocess.call("./foo2.sh", shell=True);
 wumpus_r_us_list.append('Rope');
 print (wumpus_r_us_list);
