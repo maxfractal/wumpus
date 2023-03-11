d1259d07a8bebf301d764715710ba7a5d2d7fd8c
Xris <cp@maxfractal.com>
2023-03-11 13:31:41 -0500

    adding weapon objects from game.py

--- game/game.py
+++ game/game.py
@@ -1,5 +1,37 @@
+
+class Weapon:
+    def _str_(self):
+        return self.name
+
+class Rock:
+    def _init_(self):
+        self.name = "Rock"
+        self.description = "A fist-sized rock, suitable for bludgeoning."
+        self.damage = 5
+
+    def _str_(self):
+        return self.name
+    
+class Dagger:
+    def _init_(self):
+        self.name = "Dagger"
+        self.description = "A small dagger with some rust. "  "Somewhat more dangerous than a rock."
+        self.damage = 10
+
+    def _str_(self):
+        return self.name
+    
+class RustySword:
+    def _init_(self):
+        self.name = "Rusty Sword"
+        self.description = "This sword is showing its age, " "but still has some fight left in it."
+        self.damage = 20
+
+    def _str_(self):
+        return self.name
+
 def play():
-    inventory = ['Dagger', 'Gold(5)', 'Crusty Bread']
+    inventory = [Rock(), 'Gold(5)', 'Crusty Bread']
     print("Escape from Cave Terror")
     while True:
         action_input = get_player_command()
