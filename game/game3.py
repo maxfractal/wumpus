8ad9fbc577230dfa3c8e3b231e8f0a5e675e2da3
Xris <cp@maxfractal.com>
2023-05-04 15:57:35 -0400

    object name display issue resolved, still working on having classes defined in a seperate file.

---
+++ game/game3.py
@@ -0,0 +1,34 @@
+#this version is like the book at this point
+import weapons
+
+
+r1 = Rock()
+d1 = Dagger()
+
+def play():
+    inventory = [r1().name, 'Gold(5)', 'Crusty Bread']
+    print("Escape from Cave Terror")
+    while True:
+        action_input = get_player_command()
+        if action_input in ['n','N','north','Nouth']:
+            print("Go North!")
+        elif action_input in ['s','S','south','South']:
+            print("Go South!")
+        elif action_input in ['e','E','east','East']:
+            print("Go East!")
+        elif action_input in ['w','W','west','West']:
+            print("Go West!")
+        elif action_input in ['i','I','inventory','Inventory']:
+            print("Inventory:")
+            #print(inventory)
+            for item in inventory:
+                print('*' + str(item))
+        else:
+            print("Invalid action!")
+            print('Bye!')
+            exit()
+
+def get_player_command():
+        return input('Action: ')
+
+play()
