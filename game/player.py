7b6871b4fb4e7c6ae5e5b31ff00cc1335a3aac5e
Xris <cp@maxfractal.com>
2024-03-24 14:09:20 -0400

    Intermezzo working
    World map added. Appears to be in working order.

--- game/player.py
+++ game/player.py
@@ -1,8 +1,27 @@
-import weapons
+import items
 
 class Player:
     def __init__(self):
-        self.inventory = [weapons.Rock(), weapons.Dagger(), weapons.RustySword(), 'Gold(5)', 'Crusty Bread']
+        self.inventory = [items.Rock(), items.Dagger(), items.RustySword(), 'Gold(5)', 'Crusty Bread']
+
+        self.x = 1
+        self.y = 2
+    
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def move_north(self):
+        self.move(dx=0,dy=-1)
+
+    def move_south(self):
+        self.move(dx=0,dy=1)
+
+    def move_east(self):
+        self.move(dx=1,dy=0)
+
+    def move_west(self):
+        self.move(dx=-1,dy=0)
 
     def print_inventory(self):
         print("Inventory:")
