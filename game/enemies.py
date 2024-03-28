e9d53d3e7ffca8662461908c7f97907bc9eeba95
Xris <cp@maxfractal.com>
2024-03-28 09:00:32 -0400

    Enenmies object added to game.

---
+++ game/enemies.py
@@ -0,0 +1,15 @@
+class Enemy:
+    def __init__(self):
+        raise NotImplementedError("Do not create raw Enemy objects.")
+    
+    def __str__(self):
+        return self.name
+    
+    def is_alive(self):
+        return self.hp > 0
+    
+class GiantSpider(Enemy):
+    def __init__(self):
+        self.name = "Giant Spider"
+        self.hp = 10
+        self.damage = 2
